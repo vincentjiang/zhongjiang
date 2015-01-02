0f33a60c51b12db34e98278089ccd439612d7250
VincentJiang <jxs471494539@gmail.com>
2015-01-02 14:41:55 +0800

    udpate cms products form

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150102032322) do
-
-  create_table "images", force: :cascade do |t|
-    t.integer  "product_id", limit: 4, null: false
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-  end
-
-  add_index "images", ["product_id"], name: "index_images_on_product_id", using: :btree
+ActiveRecord::Schema.define(version: 20150102061557) do
 
   create_table "zj_categories", force: :cascade do |t|
     t.string   "name",        limit: 255, null: false
@@ -29,6 +21,14 @@ ActiveRecord::Schema.define(version: 20150102032322) do
   end
 
   add_index "zj_categories", ["category_id"], name: "index_zj_categories_on_category_id", using: :btree
+
+  create_table "zj_images", force: :cascade do |t|
+    t.integer  "product_id", limit: 4, null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "zj_images", ["product_id"], name: "index_zj_images_on_product_id", using: :btree
 
   create_table "zj_products", force: :cascade do |t|
     t.string   "title",      limit: 255,   null: false
@@ -48,5 +48,5 @@ ActiveRecord::Schema.define(version: 20150102032322) do
     t.datetime "updated_at",                                  null: false
   end
 
-  add_foreign_key "images", "zj_products", column: "product_id"
+  add_foreign_key "zj_images", "zj_products", column: "product_id"
 end
